cd0674bc4e6180297ca22775443766e0bf163685
Stanislav Heller <heller.stanislav@gmail.com>
2013-10-28 11:08:29 +0100

    New structure for configuration which intelligently merges settings.
    If there was regular dict, default settings would overwrite settings returned
    by EveMongoengine.create_settings method, which is not cool at all.

---
+++ eve_mongoengine/struct.py
@@ -0,0 +1,37 @@
+
+"""
+    struct
+    ~~~~~~
+
+    Datastructures for eve-mongoengine.
+
+    :copyright: (c) 2013 by Stanislav Heller.
+    :license: BSD, see LICENSE for more details.
+"""
+
+def _merge_dicts(d1, d2):
+    """
+    Helper function for merging dicts. This functino is called in
+    :func:`Settings.update`.
+    """
+    for key, value in d2.iteritems():
+        if key in d1:
+            if isinstance(d1[key], dict) and isinstance(value, dict):
+                _merge_dicts(d1[key], value)
+                continue
+        d1[key] = value
+
+class Settings(dict):
+    """
+    Special mergable dictionary for eve settings. Used as config keeper
+    returned by method :func:`EveMongoengine.create_settings`.
+
+    The difference between Settings object and default dict is that update()
+    method in Settings does not overwrite the key when value is dictionary,
+    but tries to merge inner dicts in an intelligent way.
+    """
+    def update(self, other):
+        """Update method, which respects dictionaries recursively."""
+        _merge_dicts(self, other)
+
+__all__ = [Settings]
---
+++ tests/test_struct.py
@@ -0,0 +1,84 @@
+
+import unittest
+from copy import deepcopy
+
+from eve import Eve
+from mongoengine import Document, StringField, ListField, IntField
+
+from eve_mongoengine.struct import Settings
+from eve_mongoengine import EveMongoengine
+
+
+class TestSettingsDict(unittest.TestCase):
+    def setUp(self):
+        self.d = Settings({
+            'a': 1,
+            'b': "hello",
+            'c': [1,2,3],
+            'd': {'x': 'y', 'y': 5},
+            'e': {'q': [1,2,{'e':5}], 'w': {'r':'s', 't':'u'}}
+        })
+        self.g = dict(deepcopy(self.d))
+    
+    def check(self):
+        self.assertDictEqual(self.d, self.g)
+
+    def test_simple(self):
+        self.d.update({'a': 2})
+        self.g['a'] = 2
+        self.check()
+        self.d.update({'b': "cheers"})
+        self.g['b'] = "cheers"
+        self.check()
+
+    def test_list(self):
+        self.d.update({'c': [1,2,4]})
+        self.g['c'] = [1,2,4]
+        self.check()
+        self.d.update({'c': 4})
+        self.g['c'] = 4
+        self.check()
+        self.d.update({'e': {'q': [1,2]}})
+        self.g['e']['q'] = [1,2]
+        self.check()
+
+    def test_dict(self):
+        self.d.update({'d': {'x': 'z'}})
+        self.g['d'] = {'x': 'z', 'y': 5}
+        self.check()
+
+    def test_nested_dict(self):
+        self.d.update({'e': {'w': {'r': 5}}})
+        self.g['e']['w']['r'] = 5
+        self.check()
+
+
+class A(Document):
+    x = StringField()
+    y = ListField(IntField())
+
+DEFAULT_SETTINGS = {
+    'DOMAIN': {'a': {'schema': {'x': {'type': 'int'}}}}
+}
+
+class TestCreateSettings(unittest.TestCase):
+    def test_schema_merge(self):
+        ext = EveMongoengine()
+        settings = ext.create_settings(A)
+        settings.update(DEFAULT_SETTINGS)
+        expected = {
+          'DOMAIN': {
+            'a': {
+              'schema': {
+                'y': {'type': 'list'},
+                'x': {'type': 'int'} # NOT StringField
+              }
+            }
+          },
+          'ITEM_METHODS': ['GET', 'PATCH', 'PUT', 'DELETE'],
+          'RESOURCE_METHODS': ['GET', 'POST', 'DELETE']
+        }
+        self.assertDictEqual(settings, expected)
+
+if __name__ == "__main__":
+    unittest.main()
