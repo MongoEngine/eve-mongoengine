0645df7352aaa02f32a431d4369dae63cf876623
Sha <wangsha@users.noreply.github.com>
2015-12-08 18:22:57 +0800

    Update setup.py
    bumpversion

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Eve-Mongoengine',
-    version='0.0.9.3',
+    version='0.0.9.4',
     url='https://github.com/hellerstanislav/eve-mongoengine',
     author='Stanislav Heller',
     author_email='heller.stanislav@gmail.com',
